f174404471ed28e17502d25e5e05f3294858ee62
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-05-15 14:19:42 +0100

    header socials

---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://www.linkedin.com/in/maryann-alonge-0946ba1a3/" target='_blank'></a>
+      
+    </div>
+  )
+}
+
+export default HeaderSocials
