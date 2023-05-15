5f282e79a02bb3edb7cb813be305778ed4ed391e
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-05-15 14:04:03 +0100

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./header.css"
+import CTA from "./CTA"
 
 const Header = () => {
   return (
     <header>
-      
+      <div className="container header__container">
+        <h5>Hello! I'm</h5>
+        <h1>Maryann Olajumoke</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+      </div>
     </header>
   )
 }
